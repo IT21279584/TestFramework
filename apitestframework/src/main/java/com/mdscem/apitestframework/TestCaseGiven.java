c0837c70899d9d3cf3290ce45782f09adeed64ab
IT21279584 <it21279584@my.sliit.lk>
2024-10-05 05:46:53 -0400

    Commit

--- apitestframework/src/main/java/com/mdscem/apitestframework/TestCaseGiven.java
+++ apitestframework/src/main/java/com/mdscem/apitestframework/TestCaseGiven.java
@@ -3,10 +3,12 @@ package com.mdscem.apitestframework;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Map;
+
 @Getter
 @Setter
 public class TestCaseGiven {
-    private TestCaseHeaders headers;
+    private Map headers;
     private TestCasePathParam pathParam;
 
 }
