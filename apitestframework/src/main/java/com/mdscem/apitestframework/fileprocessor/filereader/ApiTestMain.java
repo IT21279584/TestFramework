d97771415a3d731e82d87bf1c6a91247d997582f
Kavinda-B-Medagoda <kavindabmedagoda@gmail.com>
2024-10-09 02:57:41 +0530

    includes adding part modified

--- apitestframework/src/main/java/com/mdscem/apitestframework/fileprocessor/filereader/ApiTestMain.java
+++ apitestframework/src/main/java/com/mdscem/apitestframework/fileprocessor/filereader/ApiTestMain.java
@@ -1,16 +1,22 @@
 package com.mdscem.apitestframework.fileprocessor.filereader;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.mdscem.apitestframework.fileprocessor.yamlprocessor.YamlPlaceholderReplacer;
+import org.apache.log4j.BasicConfigurator;
 
 import java.util.List;
 
 public class ApiTestMain {
 
     public static void main(String[] args) throws Exception {
-
+        BasicConfigurator.configure();
+        YamlPlaceholderReplacer replacer = new YamlPlaceholderReplacer();
+        String propertyValues = "/home/kmedagoda/Documents/Kavinda Final/final TestFramework/TestFramework/apitestframework/src/main/resources/values.yaml";
+        String yamlValues = "/home/kmedagoda/Documents/Kavinda Final/final TestFramework/TestFramework/apitestframework/src/main/resources/testcase1.yaml";
+        replacer.replaceYamlPlaceholders(yamlValues,propertyValues);
         List<String> testCaseFiles= FileConfigLoader.loadTestCasesFiles();
         JsonNode jsonNode = FileConfigLoader.readFile(testCaseFiles);
-        System.out.println(jsonNode.toPrettyString());
+//        System.out.println(jsonNode.toPrettyString());
 
     }
 }
