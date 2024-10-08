4216909561fb37c0cba56815b7df578bec4804c8
Kavinda-B-Medagoda <kavindabmedagoda@gmail.com>
2024-10-09 00:33:50 +0530

    includes adding part done

--- apitestframework/src/main/java/com/mdscem/apitestframework/fileprocessor/filereader/ApiTestMain.java
+++ apitestframework/src/main/java/com/mdscem/apitestframework/fileprocessor/filereader/ApiTestMain.java
@@ -2,16 +2,22 @@ package com.mdscem.apitestframework.fileprocessor.filereader;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.mdscem.apitestframework.fileprocessor.fileinterpreter.FileInterpreter;
+import com.mdscem.apitestframework.fileprocessor.yamlprocessor.YamlPlaceholderReplacer;
 
 import java.util.List;
 
 public class ApiTestMain {
 
     public static void main(String[] args) throws Exception {
+        YamlPlaceholderReplacer replacer = new YamlPlaceholderReplacer();
+        String yamlFilePath = "/home/kmedagoda/Documents/Kavinda Final/final TestFramework/TestFramework/apitestframework/src/main/resources/testcase1.yaml";
+        String propertiesFilePath = "/home/kmedagoda/Documents/Kavinda Final/final TestFramework/TestFramework/apitestframework/src/main/resources/values.yaml";
+        replacer.replaceYamlPlaceholders(yamlFilePath, propertiesFilePath);
+
 
         List<String> testCaseFiles= FileConfigLoader.loadTestCasesFiles();
         JsonNode jsonNode = FileConfigLoader.readFile(testCaseFiles);
-        System.out.println(jsonNode.toPrettyString());
+//        System.out.println(jsonNode.toPrettyString());
 //        System.out.println(FileInterpreter.interpret(jsonNode).get(0));
 
     }
