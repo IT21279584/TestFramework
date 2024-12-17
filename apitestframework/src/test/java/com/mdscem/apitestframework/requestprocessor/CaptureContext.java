5c59f161ae27cf213cd3d933c824297aa5516217
IT21279584 <it21279584@my.sliit.lk>
2024-12-17 00:57:51 -0500

    Update with captures and testcases 12/16

--- apitestframework/src/test/java/com/mdscem/apitestframework/requestprocessor/CaptureContext.java
+++ apitestframework/src/test/java/com/mdscem/apitestframework/requestprocessor/CaptureContext.java
@@ -2,12 +2,10 @@ package com.mdscem.apitestframework.requestprocessor;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Component
 public class CaptureContext {
 
     private static final Logger logger = LogManager.getLogger(CaptureContext.class);
--- apitestframework/src/test/java/com/mdscem/apitestframework/requestprocessor/CaptureValidation.java
+++ apitestframework/src/test/java/com/mdscem/apitestframework/requestprocessor/CaptureValidation.java
@@ -3,7 +3,6 @@ package com.mdscem.apitestframework.requestprocessor;
 import com.mdscem.apitestframework.fileprocessor.filereader.model.TestCase;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
